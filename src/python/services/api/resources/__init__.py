8ea67470c36529d62415d1d201e84835e2dd6d8f
MikeLi <18688161372lz@gmail.com>
2020-12-03 10:31:59 +0800

    Yelp api finish (bug fix_V3)

--- src/python/services/api/resources/__init__.py
+++ src/python/services/api/resources/__init__.py
@@ -1,4 +1,4 @@
 from .healthcheck import HealthCheckAPI  # noqa: F401
 from .audit import AuditAPI  # noqa: F401
 from .auth import LoginAPI, RegisterAPI, LogoutAPI, UserAPI, GithubOAuthLoginAPI, GithubOAuthAuthorizeAPI  # noqa: F401 E501
-from .yelp import YelpAPI
+# from .yelp import YelpAPI
